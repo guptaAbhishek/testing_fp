6d6e8f84cf4a387b95f579505989766a1a556790
Abhishek Kumar Gupta <abhishek.gupta@hindustantimes.com>
2018-04-11 15:41:46 +0530

    fp testing

---
+++ env/amp/index.js
@@ -0,0 +1,44 @@
+(function(){
+
+    'use strict';
+
+    var express = require('express');
+
+    var app = express();
+
+    var request = require('request');
+
+    var path = require('path');
+
+    var engines = require('consolidate');
+
+    app.engine('html', engines.mustache);
+
+    app.set('view engine', 'html');
+
+    app.use(express.static(__dirname + '/public'));
+
+    app.set('views', path.join(__dirname, '/public'));
+
+    app.use(express.static('views'));
+
+    app.set('port', (process.env.PORT || 8131));
+
+
+
+    app.use(function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+    });
+
+    app.get('/',function(req,res){
+        res.render('amp.html');
+    });
+
+
+
+    app.listen(app.get('port'), function() {
+        console.log("Node app is running at localhost:" + app.get('port'))
+    });
+
+})();
